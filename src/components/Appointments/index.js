c561645d25626e25a43b007014b4052cdbdaa495
Matt-Corsetti <32616359+Matt-Corsetti@users.noreply.github.com>
2020-03-08 11:39:30 -0400

    Getting ready to merge

--- src/components/Appointments/index.js
+++ src/components/Appointments/index.js
@@ -27,8 +27,6 @@ import {
 
 import MaterialTable from "material-table";
 
-import nodemailer from "nodemailer";
-
 import "./Appointments.css";
 
 import validationSchema from "./validation";
@@ -124,30 +122,6 @@ function AppointmentsPage() {
     setOpen(true);
   };
 
-  const sendAppointmentEmail = () => {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: "matt.j.corsetti@gmail.com",
-        pass: "Reboundleader-15"
-      }
-    });
-
-    const mailOptions = {
-      from: "matt.j.corsetti@gmail.com",
-      to: "matt.j.corsetti@gmail.com",
-      subject: "Appointment Confirmation",
-      text: "Your appointment has been created."
-    };
-
-    transporter.sendMail(mailOptions, function(error, info) {
-      if (error) {
-        console.log(error);
-      } else {
-        console.log("Email sent: " + info.response);
-      }
-    });
-  };
   const [addAppointment] = useMutation(ADD_APPOINTMENTS);
 
   return (
@@ -354,79 +328,6 @@ function AppointmentsPage() {
         </Grid>
       </div>
     </div>
-
-    // <div className="form-container">
-    //   <Form
-    //     onSubmit={e => {
-    //       e.preventDefault();
-    //       addAppointment({
-    //         variables: {
-    //           userEmail: userEmail,
-    //           appointmentDate: appointmentDate,
-    //           appointmentTime: appointmentTime,
-    //           appointmentType: appointmentType
-    //         }
-    //       });
-    //     }}
-    //   >
-    //     <Form.Row>
-    //       <Form.Group as={Col} md="12">
-    //         <Form.Label>Email</Form.Label>
-    //         <Form.Control
-    //           required
-    //           type="email"
-    //           value={userEmail}
-    //           onChange={e => setUserEmail(e.target.value)}
-    //           placeholder="Email"
-    //         />
-    //       </Form.Group>
-    //     </Form.Row>
-    //     <Form.Row>
-    //       <Form.Group as={Col} md="12">
-    //         <Form.Label>Appointment Date</Form.Label>
-    //         <Form.Control
-    //           required
-    //           type="text"
-    //           value={appointmentDate}
-    //           onChange={e => setAppointmentDate(e.target.value)}
-    //           placeholder="Appointment Date"
-    //         />
-    //       </Form.Group>
-    //     </Form.Row>
-    //     <Form.Row>
-    //       <Form.Group as={Col} md="12">
-    //         <Form.Label>Appointment Time</Form.Label>
-    //         <Form.Control
-    //           required
-    //           type="text"
-    //           value={appointmentTime}
-    //           onChange={e => setAppointmentTime(e.target.value)}
-    //           placeholder="Appointment Time"
-    //         />
-    //       </Form.Group>
-    //     </Form.Row>
-    //     <Form.Row>
-    //       <Form.Group as={Col} md="12">
-    //         <Form.Label>Appointment Type</Form.Label>
-    //         <Form.Control
-    //           required
-    //           type="text"
-    //           value={appointmentType}
-    //           onChange={e => setAppointmentType(e.target.value)}
-    //           placeholder="Appointment Type"
-    //         />
-    //       </Form.Group>
-    //     </Form.Row>
-    //     <Form.Group>
-    //       <div className="btn-submit">
-    //         <Button type="submit">Save</Button>
-    //       </div>
-    //       <div className="btn-cancel">
-    //         <Button variant="danger ">Cancel</Button>
-    //       </div>
-    //     </Form.Group>
-    //   </Form>
-    // </div>
   );
 }
 
